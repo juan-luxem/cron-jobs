711961d3bdaa11919732cbb598d5fa198681f8ba
GomenJ <juan.gonzalez.mendoza.j@gmail.com>
2025-08-29 11:56:36 -0600

    refactor: start new  cron job

---
+++ config.py
@@ -0,0 +1,39 @@
+from pydantic import EmailStr, SecretStr, HttpUrl, Field, ValidationError
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    """
+    Defines and validates application environment variables.
+    """
+    model_config = SettingsConfigDict(
+        env_file='.env', 
+        env_file_encoding='utf-8',
+        extra='ignore' # Ignores extra variables in the .env file
+    )
+
+    # NGI Credentials
+    NGI_EMAIL: EmailStr
+    NGI_PASSWORD: SecretStr = Field(..., min_length=3)
+    NGI_ENDPOINT: HttpUrl
+    API_URL: HttpUrl
+
+    # Telegram Bot Tokens and IDs
+    TELEGRAM_BOT_GAS_NOTIFIER_TOKEN: SecretStr = Field(..., min_length=11)
+    TELEGRAM_BOT_MERCADOS_LUX_TOKEN: SecretStr = Field(..., min_length=11)
+    
+    TELEGRAM_GROUP_CHAT_ID: str = Field(..., min_length=5)
+    TELEGRAM_CHAT_ID: str = Field(..., min_length=5)
+
+    MAU_CREDENTIALS_PASSWORD: SecretStr = Field(..., min_length=4)
+    MAU_USERNAME: str = Field(..., min_length=4)
+    MAU_PASSWORD: SecretStr = Field(..., min_length=4)
+
+
+try:
+    ENV = Settings()
+except ValidationError as e:
+    print("🔥 Error: Environment variable validation failed!")
+    print(e)
--- cron.py
+++ cron.py
@@ -1,15 +1,16 @@
 import time
 import signal
 from apscheduler.schedulers.background import BackgroundScheduler
-from ngi_api import ngi
-from mda_mtr import pml_mda, pml_mtr, pnd_mda, pnd_mtr
-from demanda import demanda
+# from ngi_api import ngi
+# from mda_mtr import pml_mda, pml_mtr, pnd_mda, pnd_mtr
+# from demanda import demanda
 import logging
 import os
-from dotenv import load_dotenv
-from rea_service import rea
-from capacidad_transferencia import capacidad_transferencia
-from demanda_real_balance import demanda_real_balance
+# from rea_service import rea
+# from capacidad_transferencia import capacidad_transferencia
+# from demanda_real_balance import demanda_real_balance
+# from generacion_ofertada import generacion_ofertada_mda, generacion_ofertada_mtr_expost
+# from generacion_hidro_ofertada import generacion_hidro_ofertada_diaria_mda, generacion_hidro_ofertada_diaria_mtr_expot
 
 # Ensure the logs directory exists
 os.makedirs("./logs", exist_ok=True)
@@ -30,8 +31,6 @@ if __name__ == "__main__":
         level=logging.INFO,
     )
 
-    load_dotenv()
-
 
     # scheduler.add_job(
     #     demanda.obtener_demanda,
@@ -49,47 +48,47 @@ if __name__ == "__main__":
     #     minute="58" # every 30 minutes
     # )
     
-    scheduler.add_job(
-        capacidad_transferencia.get_capacidad_transferencia,
-        "cron",
-        day_of_week="*",
-        # hour="9",
-        # minute="0"
-        hour="8",
-        minute="19"
-    )   
+    # scheduler.add_job(
+    #     capacidad_transferencia.get_capacidad_transferencia,
+    #     "cron",
+    #     day_of_week="*",
+    #     # hour="9",
+    #     # minute="0"
+    #     hour="8",
+    #     minute="19"
+    # )   
 
-    scheduler.add_job(
-        demanda_real_balance.obtener_demanda_real_balance,   
-        "cron",
-        day_of_week="*", # every day
-        hour="15", # Every day at 3 PM
-        minute="15" # Every day at 3:15 PM
-    )   
-    
-    scheduler.add_job(
-        pml_mda.get_pml_mda,
-        "cron",
-        day_of_week="*",
-        hour="8",
-        minute="29"
-       )
+    # scheduler.add_job(
+    #     demanda_real_balance.obtener_demanda_real_balance,   
+    #     "cron",
+    #     day_of_week="*", # every day
+    #     hour="15", # Every day at 3 PM
+    #     minute="15" # Every day at 3:15 PM
+    # )   
+     
+    # scheduler.add_job(
+    #     pml_mda.get_pml_mda,
+    #     "cron",
+    #     day_of_week="*",
+    #     hour="8",
+    #     minute="29"
+    #    )
 
-    scheduler.add_job(
-        pml_mtr.get_pml_mtr,
-        "cron",
-        day_of_week="*",
-        hour="8",
-        minute="5"
-       )
+    # scheduler.add_job(
+    #     pml_mtr.get_pml_mtr,
+    #     "cron",
+    #     day_of_week="*",
+    #     hour="8",
+    #     minute="5"
+    #    )
 
-    scheduler.add_job(
-        pnd_mda.get_pnd_mda,
-        "cron",
-        day_of_week="*",
-        hour="8",
-        minute="32"
-       )
+    # scheduler.add_job(
+    #     pnd_mda.get_pnd_mda,
+    #     "cron",
+    #     day_of_week="*",
+    #     hour="8",
+    #     minute="32"
+    #    )
 
     # scheduler.add_job(
     #     pnd_mtr.get_pnd_mtr,
@@ -97,6 +96,39 @@ if __name__ == "__main__":
     #     day_of_week="*",
     #     hour="8",
     #     minute="11",
+    # )
+
+    ## Cron para ofertas MDA y MTR Expost
+    # scheduler.add_job(
+    #     generacion_hidro_ofertada_diaria_mda.obtener_generacion_hidro_ofertada_diaria_mda,
+    #     "cron",
+    #     day_of_week="*", # every day
+    #     hour="6", # Every day at 6 AM
+    #     minute="0" # Every day at 6:00 AM
+    # )
+
+    # scheduler.add_job(
+    #     generacion_hidro_ofertada_diaria_mtr_expot.obtener_generacion_hidro_ofertada_diaria_mtr,
+    #     "cron",
+    #     day_of_week="*", # every day
+    #     hour="6", # Every day at 6 AM
+    #     minute="5" # Every day at 6:05 AM
+    # )
+
+    # scheduler.add_job(
+    #     generacion_ofertada_mda.obtener_generacion_ofertada_mda,
+    #     "cron",
+    #     day_of_week="*", # every day
+    #     hour="6", # Every day at 6 AM
+    #     minute="15" # Every day at 6:15 AM
+    # )
+
+    # scheduler.add_job(
+    #     generacion_ofertada_mtr_expost.obtener_generacion_ofertada_mtr_expost,
+    #     "cron",
+    #     day_of_week="*", # every day
+    #     hour="6", # Every day at 6 AM
+    #     minute="30" # Every day at 6:30 AM
     # )
 
     logging.info("process started")
