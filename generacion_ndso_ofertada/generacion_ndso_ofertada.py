d90a41eeb1305966a8879039ebbc06f0c19e3ed4
Juan gomen <gomen98.jg@gmail.com>
2025-12-09 17:34:34 -0600

    feat: Implement generic function for downloading Ofertas No Despachables data and add related constants

--- generacion_ndso_ofertada/generacion_ndso_ofertada.py
+++ generacion_ndso_ofertada/generacion_ndso_ofertada.py
@@ -1,13 +1,15 @@
-from .get_generic_generacion_ndso_ofertada import get_generacion_ndso_ofertada_generic
+from .get_generacion_ndso_ofertada import get_generacion_ndso_ofertada
 from .process_generacion_ndso_ofertada import process_generacion_ndso_ofertada
 from global_utils import delete_csv_files_after_process
 
+
 def get_generacion_ndso_ofertada_mda():
-    get_generacion_ndso_ofertada_generic('MDA')
-    process_generacion_ndso_ofertada('MDA')
+    get_generacion_ndso_ofertada("MDA")
+    process_generacion_ndso_ofertada("MDA")
     delete_csv_files_after_process()
 
+
 def get_generacion_ndso_ofertada_mtr():
-    get_generacion_ndso_ofertada_generic('MTR')
-    process_generacion_ndso_ofertada('MTR')
+    get_generacion_ndso_ofertada("MTR")
+    process_generacion_ndso_ofertada("MTR")
     delete_csv_files_after_process()
