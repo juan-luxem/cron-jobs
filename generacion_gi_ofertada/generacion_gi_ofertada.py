eb7ac0a2e1b0a2ad6e6f6952d15c85a0e819f499
Juan gomen <gomen98.jg@gmail.com>
2025-12-09 16:36:35 -0600

    feat: Add constants for URL, ID_REPORT, MIN_DATE, and VIEW_GENERATOR; implement generic function for downloading Generacion GI Ofertada data

--- generacion_gi_ofertada/generacion_gi_ofertada.py
+++ generacion_gi_ofertada/generacion_gi_ofertada.py
@@ -1,14 +1,15 @@
-from .get_generic_generacion_gi_ofertada import get_generacion_gi_ofertada_generic
+from .get_generacion_gi_ofertada import get_generacion_gi_ofertada_generic
 from .process_generacion_gi_ofertada import process_generacion_gi_ofertada
 from global_utils import delete_csv_files_after_process
 
+
 def get_generacion_gi_ofertada_mda():
-    get_generacion_gi_ofertada_generic('MDA')
-    process_generacion_gi_ofertada('MDA')
+    get_generacion_gi_ofertada_generic("MDA")
+    process_generacion_gi_ofertada("MDA")
     delete_csv_files_after_process()
 
 
 def get_generacion_gi_ofertada_mtr():
-    get_generacion_gi_ofertada_generic('MTR')
-    process_generacion_gi_ofertada('MTR')
+    get_generacion_gi_ofertada_generic("MTR")
+    process_generacion_gi_ofertada("MTR")
     delete_csv_files_after_process()
