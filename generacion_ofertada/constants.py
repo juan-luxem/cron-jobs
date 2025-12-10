d53218ae43e2eb481cc76ab2d51b65f59e02d614
Juan gomen <gomen98.jg@gmail.com>
2025-12-10 16:50:53 -0600

    feat: Add generic function for downloading Ofertas Térmicas data and related constants

---
+++ generacion_ofertada/constants.py
@@ -0,0 +1,13 @@
+URL = {
+    "MDA": "https://www.cenace.gob.mx/Paginas/SIM/Reportes/OfertasMDA.aspx",
+    "MTR": "https://www.cenace.gob.mx/Paginas/SIM/Reportes/OfertasMTR.aspx",
+}
+
+ID_REPORT = {
+    "MDA": "365,366",
+    "MTR": "376,377",
+}
+
+MIN_DATE = {"MDA": "29/01/2016", "MTR": "01/01/2017"}
+
+VIEW_GENERATOR = {"MDA": "E82FE174", "MTR": "43EB19B1"}
